c70bfb66338e8ce501cee402a9fd7e3b7b4da35a
Sean Goodrich <sean@smgoodrich.com>
2018-03-09 00:25:34 -0800

    remove notes and add to readme

--- js/wall.js
+++ js/wall.js
@@ -1,23 +1,5 @@
 function Wall (zPos, width, height, depth, columns, rows, image) {
-
-    // clean up code
-    // add to github
-    // create cube class
-        // support video, or image
-        // support each part of whole or separate
-    // create animations
-        // gravity, explode, pinpression, slideshow, scroll camera
-        // make each a separate action class with play and pause functions
-        // functions on 0 to 100 scale (like lerp)
-        // each animation will inherit from parent generic animation class
-    // in index should be able to just pick image url, rows, columns, and animation and it should work out of the box
-    // support multiple pages/animations (with scroll offset for 2nd and 3rd pages)
-    // add support for images of different aspect ratios (not just 1:1)
-    // option to set cube opacity and color (hex value, random, or match texture color)
-    // support having cubes of different sizes in wall
-    // add demo where user can alter number of columns,rows,image,color,animation
-    // support full screen
-
+    
     // make group private + get object function
     $this = this;
     this.group;
